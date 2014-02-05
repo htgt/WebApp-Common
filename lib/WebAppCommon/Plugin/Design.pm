ab5a354550b939c579c7bf516be1c030e0764334
af11_sanger <af11@sanger.ac.uk>
2014-02-05 09:35:02 +0000

    log created design attempt message at higher level

--- lib/WebAppCommon/Plugin/Design.pm
+++ lib/WebAppCommon/Plugin/Design.pm
@@ -525,7 +525,7 @@ sub c_create_design_attempt {
             )
         }
     );
-    $self->log->debug( 'Created design attempt ' . $design_attempt->id );
+    $self->log->info( 'Created design attempt ' . $design_attempt->id );
 
     return $design_attempt;
 }
